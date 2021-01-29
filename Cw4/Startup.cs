9778e62f07ae17c8a55af20c71f5aad1b7db17eb
s15935 <s15935@pjwstk.edu.pl>
2021-01-29 16:36:11 +0100

    Update Startup.cs

--- Cw4/Startup.cs
+++ Cw4/Startup.cs
@@ -19,7 +19,7 @@ namespace Cw4
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IDbStudentService, MssqlDbService>();
+            services.AddScoped<IDbStudentService, SqlDbService>();
             services.AddControllers();
         }
 
